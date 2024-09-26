0b4aedcbdda853d8e06d364df33f3df5fc092dab
Jae Jeong <jeongjj29@gmail.com>
2024-09-26 19:37:18 -0400

    update react-router-dom

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import NavBar from "./NavBar";
-import Outlet from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
---
+++ client/src/pages/AddPurchases.js
@@ -0,0 +1,13 @@
+import React from "react";
+import useFormik from "formik";
+import * as yup from "yup";
+
+function AddPurchases() {
+  return (
+    <div>
+      <h1>Add Purchases</h1>
+    </div>
+  );
+}
+
+export default AddPurchases;
