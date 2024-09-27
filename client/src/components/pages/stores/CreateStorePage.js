f48698a9ad51faf2dd653244baa39262731d2451
Brandon Collins <brandonc503@yahoo.com>
2024-09-26 21:07:06 -0400

    adds createstorepage.js

--- client/src/components/pages/stores/CreateStorePage.js
+++ client/src/components/pages/stores/CreateStorePage.js
@@ -1,12 +1,52 @@
-import React from 'react';
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
 
-const CreateStorePage = () => {
+// Validation schema
+const StoreSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(3, "Name must be at least 3 characters long")
+    .required("Store name is required"),
+});
+
+const CreateStoreForm = () => {
   return (
     <div>
-      <h1>Create Store</h1>
-      <p>Form to create a new store will go here.</p>
+      <h2>Create a New Store</h2>
+      <Formik
+        initialValues={{ name: "" }}
+        validationSchema={StoreSchema}
+        onSubmit={(values, { setSubmitting, resetForm }) => {
+          axios
+            .post("/api/stores", values)
+            .then((response) => {
+              console.log("Store created:", response.data);
+              resetForm();
+            })
+            .catch((error) => {
+              console.error("Error creating store:", error);
+            })
+            .finally(() => {
+              setSubmitting(false);
+            });
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <div>
+              <label htmlFor="name">Store Name</label>
+              <Field name="name" type="text" />
+              <ErrorMessage name="name" component="div" />
+            </div>
+            <button type="submit" disabled={isSubmitting}>
+              Create Store
+            </button>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 };
 
-export default CreateStorePage;
+export default CreateStoreForm;
