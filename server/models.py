cf921a72cfe1daabc3e8f72170775321643fa71e
Brandon Collins <brandonc503@yahoo.com>
2024-09-26 20:32:43 -0400

    changed port back

--- server/models.py
+++ server/models.py
@@ -1,45 +1,17 @@
 from sqlalchemy_serializer import SerializerMixin
-from sqlalchemy.ext.associationproxy import association_proxy
-
 from config import db
 
 
-# Models go here!
 class Item(db.Model, SerializerMixin):
     __tablename__ = "items"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
 
-    list_items = db.relationship("ListItems", backref="item", cascade="all, delete")
-    purchases = db.relationship("Purchase", backref="item", cascade="all, delete")
+    item_prices = db.relationship("ItemPrice", backref="item", cascade="all, delete")
 
     def __repr__(self):
         return f"<Item {self.name}>"
-
-
-class List(db.Model, SerializerMixin):
-    __tablename__ = "lists"
-
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable=False)
-
-    list_items = db.relationship("ListItems", backref="list", cascade="all, delete")
-
-    def __repr__(self):
-        return f"<List {self.title}>"
-
-
-class ListItems(db.Model, SerializerMixin):
-    __tablename__ = "list_items"
-
-    id = db.Column(db.Integer, primary_key=True)
-    quantity = db.Column(db.Integer, nullable=False)
-    list_id = db.Column(db.Integer, db.ForeignKey("lists.id"), nullable=False)
-    item_id = db.Column(db.Integer, db.ForeignKey("items.id"), nullable=False)
-
-    def __repr__(self):
-        return f"<ListItems: List {self.list_id}, Item {self.item_id}, Quantity {self.quantity}>"
 
 
 class Store(db.Model, SerializerMixin):
@@ -48,20 +20,19 @@ class Store(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
 
-    purchases = db.relationship("Purchase", backref="store", cascade="all, delete")
+    item_prices = db.relationship("ItemPrice", backref="store", cascade="all, delete")
 
     def __repr__(self):
         return f"<Store {self.name}>"
 
 
-class Purchase(db.Model, SerializerMixin):
-    __tablename__ = "purchases"
+class ItemPrice(db.Model, SerializerMixin):
+    __tablename__ = "item_prices"
 
     id = db.Column(db.Integer, primary_key=True)
-    purchase_date = db.Column(db.String, nullable=False)
     price = db.Column(db.Float, nullable=False)
     store_id = db.Column(db.Integer, db.ForeignKey("stores.id"), nullable=False)
     item_id = db.Column(db.Integer, db.ForeignKey("items.id"), nullable=False)
 
     def __repr__(self):
-        return f"<Purchase: Store {self.store_id}, Item {self.item_id}, Price {self.price}, Date {self.purchase_date}>"
+        return f"<ItemPrice: Store {self.store_id}, Item {self.item_id}, Price {self.price}>"
